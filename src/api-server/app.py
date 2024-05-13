def8ccacf8eae2dc3cd48143633064a1012b8a71
Seyone Gunasingham <s.g.seyone@protonmail.com>
2024-05-13 23:28:36 +0530

    Server gives nicer error messages

--- src/api-server/app.py
+++ src/api-server/app.py
@@ -5,14 +5,41 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app, resources={r"/predict": {"origins": "http://localhost:3000"}})
 
-model = load('model-tf-idf.joblib')
+# Load models
+models = {
+    'tf-idf': load('model-tf-idf.joblib'),
+    # Add other models here
+}
+
+
+def validate_request_data(data):
+    if not data:
+        return False, {'error': 'Request data is missing'}
+    if 'title' not in data:
+        return False, {'error': 'Title is missing from request data'}
+    if 'model' not in data:
+        return False, {'error': 'Model is missing from request data'}
+    return True, None
 
 
 @app.route('/predict', methods=['POST'])
-def predict():  # put application's code here
+def predict():
     data = request.get_json()
-    title = data['title']
+    valid, error_response = validate_request_data(data)
+    # Check if request data is valid
+    if not valid:
+        return error_response, 400
 
+    title = data['title']
+    selected_model = data.get('model')
+
+    # Check if the selected model is active
+    if selected_model not in models:
+        return {'error': 'Invalid model specified'}, 400
+
+    model = models[selected_model]
+
+    # Run the prediction, and return data
     prediction = model(title)
     return {'prediction': prediction.tolist()}
 
