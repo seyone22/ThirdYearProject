9adb4bde77b6b64633d3214e1211366628cf65ef
Seyone Gunasingham <s.g.seyone@protonmail.com>
2024-05-13 22:03:52 +0530

    Server now uses tf-idf model to recommend

--- src/api-server/app.py
+++ src/api-server/app.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app, resources={r"/predict": {"origins": "http://localhost:3000"}})
 
-model = load('model.joblib')
+model = load('model-tf-idf.joblib')
 
 
 @app.route('/predict', methods=['POST'])
