054255238db21596f82d66525322bbe589d89a9d
Seyone Gunasingham <s.g.seyone@protonmail.com>
2024-05-15 23:31:34 +0530

    Made it look decent.

--- src/api-demo-app/src/app/page.js
+++ src/api-demo-app/src/app/page.js
@@ -1,41 +1,107 @@
 'use client'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from './page.module.css'
-import {useState} from "react";
-import getBookRecommendations from "@/app/services/recommender.api.service";
+import {useEffect, useState} from "react";
+import {getBookRecommendations, getModels} from "@/app/services/recommender.api.service";
+import {Button, Dropdown, Form, Toast} from "react-bootstrap";
 
 export default function Home() {
     const [bookName, setBookName] = useState('');
     const [recommendations, setRecommendations] = useState([]);
     const [loading, setLoading] = useState(false);
+    const [modelList, setModelList] = useState([]);
+    const [selectedModel, setSelectedModel] = useState('fuzzy-tf-idf');
+    const [gotData, setGotData] = useState('');
 
-    const handleRecommendation = async () => {
+    const [showToast, setShowToast] = useState(false);
+
+    useEffect(() => {
+        if (showToast) {
+            const timer = setTimeout(() => {
+                setShowToast(false);
+            }, 3000); // Assuming you want to reset success after 3 seconds (adjust as needed)
+            return () => clearTimeout(timer);
+        }
+    }, [showToast]);
+
+    useEffect(() => {
+        getModels().then(data => {
+            setModelList(data);
+        });
+    }, []);
+
+    const handleRecommendation = async (e) => {
+        e.preventDefault()
         setLoading(true);
-        const recommendedBooks = await getBookRecommendations(bookName);
-        setRecommendations(recommendedBooks);
+        const data = await getBookRecommendations(bookName, selectedModel);
+        console.log(data);
+        if (Array.isArray(data)) {
+            setRecommendations(data);
+        } else {
+            setGotData(data)
+            setShowToast(true)
+        }
         setLoading(false);
-    }
+    };
+
+    const handleSelect = (eventKey) => {
+        setSelectedModel(eventKey);
+    };
 
     return (
         <main className={styles.main}>
+            {showToast && (
+                <Toast className={styles.toast}>
+                    <Toast.Header >
+                        <strong className="mr-auto">Error:</strong>
+                    </Toast.Header>
+                    <Toast.Body>{gotData.toString()}</Toast.Body>
+                </Toast>
+            )}
 
-            <div>
+            <div className={styles.title}>
+                <h1>Recommendation API Demo</h1>
+            </div>
+            <div className={styles.inputArea}>
                 <div>
-                    <input
-                        type="text"
-                        placeholder="Enter book name"
-                        value={bookName}
-                        onChange={(e) => setBookName(e.target.value)}
-                    />
-                    <button onClick={handleRecommendation} disabled={loading}>
-                        {loading ? 'Fetching...' : 'Recommend'}
-                    </button>
+                    <Form>
+                        <Form.Control
+                            className={styles.input}
+                            value={bookName}
+                            onChange={(e) => setBookName(e.target.value)}
+                            type="text"
+                            name='bookName'
+                            placeholder="Book Name"
+                            onSubmit={(e) => handleRecommendation(e)}
+                        />
+
+                        <Form.Label>
+                            Model:
+                        </Form.Label>
+                        <Form.Select className={styles.input} onChange={(e) => handleSelect(e.target.value)} aria-label="Default select example">
+                            {modelList.map(item => (
+                                <option key={item.id} value={item}>{item}</option>
+                            ))}
+                        </Form.Select>
+
+                        <Button
+                            className={styles.button}
+                            variant="primary"
+                            onClick={(e) => handleRecommendation(e)}
+                            disabled={loading}
+                        >
+                            {loading ? 'Fetching...' : 'Recommend'}
+                        </Button>
+                    </Form>
                 </div>
-                <ul>
-                    {recommendations.map((recommendation, index) => (
-                        <li key={index}>{recommendation}</li>
-                    ))}
-                </ul>
+            </div>
+            <div>
+                {recommendations.map((recommendation, index) => (
+                    <div className={styles.card} key={recommendation}>
+                        <h5>{recommendation}</h5>
+                    </div>
+                ))}
             </div>
         </main>
-    )
+    );
 }
--- src/api-demo-app/src/app/services/recommender.api.service.js
+++ src/api-demo-app/src/app/services/recommender.api.service.js
@@ -1,24 +1,38 @@
-async function getBookRecommendations(title, model='fuzzy-tf-idf') {
+export async function getBookRecommendations(title, model) {
     try {
         const response = await fetch('http://localhost:5000/predict', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({ title: title, model: model})
+            body: JSON.stringify({title: title, model: model})
         });
         const data = await response.json();
         console.log(data.prediction);
 
-        if(data.error) {
+        if (data.error) {
             throw data.error
         }
 
         return data.prediction;
     } catch (error) {
         console.error('Error fetching recommendations:', error);
-        return [];
+        return error;
     }
 }
 
-export default getBookRecommendations;
+export async function getModels() {
+    try {
+        const response = await fetch('http://localhost:5000/models', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        });
+        const data = await response.json()
+        return data.models;
+    } catch (error) {
+    console.error('Error fetching models:', error);
+    return[];
+    }
+}
